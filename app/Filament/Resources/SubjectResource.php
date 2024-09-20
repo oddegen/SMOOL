7e0dff99177da1b7148be86bb4f928bd85790530
oddegen <nahomtensae@gmail.com>
2024-09-20 09:44:43 +0300

    feat: add Subject model and resource

---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Filament\Resources\SubjectResource\RelationManagers;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('code')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime('d/m/y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->label(''),
+                Tables\Actions\DeleteAction::make()
+                    ->label(''),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make()
+                    ->label('Delete'),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSubjects::route('/'),
+        ];
+    }
+}
---
+++ database/factories/SubjectFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subject>
+ */
+class SubjectFactory extends Factory
+{
+
+    protected static ?string $name;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $subjects = [
+            'Mathematics',
+            'Science',
+            'History',
+            'Geography',
+            'English',
+            'Computer Science',
+            'Physical Education',
+            'Art',
+            'Music',
+            'Drama'
+        ];
+
+        static $index = 0;
+
+        if ($index >= count($subjects)) {
+            throw new \Exception('No more unique subjects available.');
+        }
+
+        $name = $subjects[$index++];
+
+        return [
+            'name' => $name,
+            'code' => match ($name) {
+                'Mathematics' => 'MATH',
+                'Science' => 'SCI',
+                'History' => 'HIST',
+                'Geography' => 'GEO',
+                'English' => 'ENG',
+                'Computer Science' => 'CS',
+                'Physical Education' => 'PE',
+                'Art' => 'ART',
+                'Music' => 'MUS',
+                'Drama' => 'DRA',
+            },
+            'created_at' => fake()->dateTimeBetween('-1 year', 'now'),
+        ];
+    }
+}
